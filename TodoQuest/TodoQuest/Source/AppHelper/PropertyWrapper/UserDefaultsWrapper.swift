b38210f7ac97bcd48bafc67696da208d29aa1b07
Crois0509 <crois0509@naver.com>
2025-08-06 17:30:50 +0900

    ✨[feat] UserDefaults Property Wrapper 구현

---
+++ TodoQuest/TodoQuest/Source/AppHelper/PropertyWrapper/UserDefaultsWrapper.swift
@@ -0,0 +1,29 @@
+//
+//  UserDefaultsWrapper.swift
+//  TodoQuest
+//
+//  Created by 장상경 on 8/6/25.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefaultsWrapper<T> {
+    let key: String
+    let defaultValue: T
+    
+    init(key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+    
+    var wrappedValue: T {
+        get {
+            return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+        }
+        
+        set {
+            UserDefaults.standard.set(newValue, forKey: key)
+        }
+    }
+}
