2553ebb3a0b6316d604f101bf095b06acb8886a5
Crois0509 <crois0509@naver.com>
2025-08-06 17:32:01 +0900

    ✨[feat] MainCoordinator 구현

--- TodoQuest/TodoQuest/Source/AppHelper/Coordinator/Coordinator.swift
+++ TodoQuest/TodoQuest/Source/AppHelper/Coordinator/Coordinator.swift
@@ -3,5 +3,17 @@
 extension AppCoordinator: LaunchCoordinatorDelegate {
     /// Launch에서 Main으로 화면전환을 요청하는 메소드
     func pushMainViewController() {
+// MARK: - MainCoordinator
+
+final class MainCoordinator: Coordinator {
+    private let nav: UINavigationController
+    
+    init(nav: UINavigationController) {
+        self.nav = nav
+    }
+    
+    func start() {
+        let vc = MainViewController()
+        nav.viewControllers = [vc]
     }
 }
