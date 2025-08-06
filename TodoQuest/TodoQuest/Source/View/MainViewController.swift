9e27a57ff416b3df4af583c3d854852ca18200ad
Crois0509 <crois0509@naver.com>
2025-08-06 20:12:56 +0900

    ✨[feat] Main 상단 탭 타이틀 설정

--- TodoQuest/TodoQuest/Source/View/MainViewController.swift
+++ TodoQuest/TodoQuest/Source/View/MainViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+import RxSwift
+import RxCocoa
 
 final class MainViewController: UIViewController {
+    
+    private let titleLogo = UIImageView().then {
+        $0.image = .title
+        $0.contentMode = .scaleAspectFit
+        $0.backgroundColor = .clear
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
     }
-
 
 }
 
+// MARK: - UI Setting Method
+
+private extension MainViewController {
+    
+    func setupUI() {
+        configureSelf()
+        setupLayout()
+    }
+    
+    func configureSelf() {
+        navigationController?.isNavigationBarHidden = false
+        navigationItem.titleView = titleLogo
+        navigationItem.setRightBarButton(createBarButton(), animated: false)
+        
+        view.backgroundColor = .CustomColors.mainWhite
+    }
+    
+    func setupLayout() {
+        
+    }
+    
+    func createBarButton() -> UIBarButtonItem {
+        return UIBarButtonItem(image: UIImage(systemName: "gearshape"), style: .plain, target: nil, action: nil).then {
+            $0.tintColor = .CustomColors.mainBlack
+        }
+    }
+    
+}
