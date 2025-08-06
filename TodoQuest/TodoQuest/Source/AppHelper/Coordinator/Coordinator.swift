134380adf812e0055cc7a1fa82b1fab6c83b8d61
Crois0509 <crois0509@naver.com>
2025-08-06 17:30:20 +0900

    ✨[feat] LaunchCoordinator Delegate 구현

---
+++ TodoQuest/TodoQuest/Source/AppHelper/Coordinator/Coordinator.swift
@@ -0,0 +1,7 @@
+// MARK: - AppCoordinator Delegate Methods
+
+extension AppCoordinator: LaunchCoordinatorDelegate {
+    /// Launch에서 Main으로 화면전환을 요청하는 메소드
+    func pushMainViewController() {
+    }
+}
