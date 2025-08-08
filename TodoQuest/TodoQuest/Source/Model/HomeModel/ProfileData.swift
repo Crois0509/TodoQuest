a761774f548161989a77e46662860b5ebc7bfd48
Crois0509 <crois0509@naver.com>
2025-08-08 16:32:05 +0900

    ✨[feat] 프로필 데이터 모델 구현

---
+++ TodoQuest/TodoQuest/Source/Model/HomeModel/ProfileData.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileData.swift
+//  TodoQuest
+//
+//  Created by 장상경 on 8/8/25.
+//
+
+import UIKit
+
+struct ProfileData {
+    let image: UIImage?
+    let name: String
+    let level: Int
+    let exp: Int
+    let progress: Double
+}
