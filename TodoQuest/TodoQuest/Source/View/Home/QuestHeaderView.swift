9141250a163b6894fdb2b1d458f153e4a3eefa27
Crois0509 <crois0509@naver.com>
2025-08-11 22:37:47 +0900

    ✨[feat] 퀘스트 테이블뷰 헤더 구현

---
+++ TodoQuest/TodoQuest/Source/View/Home/QuestHeaderView.swift
@@ -0,0 +1,31 @@
+//
+//  QuestHeaderView.swift
+//  TodoQuest
+//
+//  Created by 장상경 on 8/11/25.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+final class QuestHeaderView: UILabel {
+    
+    static let id = "QuestHeaderView"
+    
+    init(_ headerTitle: String?) {
+        super.init(frame: .zero)
+        text = headerTitle
+        font = .SCDream(size: 20, weight: .bold)
+        textColor = .Label.blackLabel
+        textAlignment = .left
+        backgroundColor = .clear
+        numberOfLines = 1
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
